c5969765a8de60f606ac6658047e0f22bd06ff7f
Fergal Mc Carthy <fmccarthy@suse.com>
2025-06-23 17:22:41 -0400

    Add support for performing DB migrations
    Add a dbversion table used to keep track of database migrations.
    
    Add initial dummy migrations to set the initial dbversion.

--- app/database/operationaldb/database.go
+++ app/database/operationaldb/database.go
@@ -7,6 +7,7 @@ import (
 
 // Operational DB Tables
 var operationalDbTables = database.DbTables{
+	database.GetDbVersionTableSpec(),
 	database.GetReportsStagingTableSpec(),
 	database.GetClientsTableSpec(),
 }
@@ -15,11 +16,34 @@ func GetTables() database.DbTables {
 	return operationalDbTables
 }
 
+//
+// Operational DB Migration
+//
+
+// list of migration versions in order from oldest to newest
+var operationalDbMigrations = database.DbMigrations{
+	&database.DbVersionMigration{
+		Version:  "046c87f9-ec02-44a2-b2f5-57498eb35fff",
+		Date:     "2025-06-02",
+		Migrator: OperationalMigrator_20250602_046c87f9,
+	},
+}
+
+func OperationalMigrator_20250602_046c87f9(adb *database.AppDb) (err error) {
+	// dummy initial migration
+	return
+}
+
+func GetMigrations() database.DbMigrations {
+	return operationalDbMigrations
+}
+
 // Get a new Operational AppDb instance
 func New(cfg *config.Config) (*database.AppDb, error) {
 	return database.GetDb(
 		"Operational",
 		&cfg.DataBases.Operational,
 		GetTables(),
+		GetMigrations(),
 	)
 }
