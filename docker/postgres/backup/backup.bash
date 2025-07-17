a22ef3489a4af5e60ee4406d2da5f70521a50b69
Gabriel Bueno <gabriel.bueno@suse.com>
2025-07-17 13:15:23 -0300

    fix backup script

--- docker/postgres/backup/backup.bash
+++ docker/postgres/backup/backup.bash
@@ -8,7 +8,7 @@ DBS=(operational telemetry)
 for DB in "${DBS[@]}"; do
     echo "Backing up $DB..."
     BACKUP_FILE="/backup/${DB}-$(date +%Y%m%d-%H%M%S).sql.gz"
-    pg_dump -h telemetry-postgres -U "$POSTGRES_USER" "$DB" | gzip > "$BACKUP_FILE"
+    pg_dump -h "${POSTGRES_HOST}" -U "$POSTGRES_USER" "$DB" | gzip > "$BACKUP_FILE"
     echo "Uploading $BACKUP_FILE to S3..."
     aws s3 cp "$BACKUP_FILE" "s3://$AWS_BUCKET/backups/$DB/$(basename "$BACKUP_FILE")"
 done
